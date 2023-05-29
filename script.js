a143fa902b9ec85aa89939b2b6bef37892aa0287
SHIVAM ASATI <49972593+shivam019@users.noreply.github.com>
2023-05-29 16:16:31 +0530

    add

--- script.js
+++ script.js
@@ -1,99 +1,97 @@
 function generateResume() {
 
+  // Social Section Name, Mob, Links etc
 
-    // document.getElementById("namep").innerHTML = namef;
-    document.getElementById("mobilep").innerHTML = mobf;
-    document.getElementById("emailp").innerHTML = emailf;
-    document.getElementById("addressp").innerHTML = addressf;
+  document.getElementById("namep").innerHTML = document.getElementById(
+    "namef"
+  ).value;
 
-    let namef = document.getElementById("namef").value;
-    let namep = document.getElementById("namep");
-     namep.innerHTML = namef;
+  document.getElementById("namep1").innerHTML = document.getElementById(
+    "namef"
+  ).value;
+
+  document.getElementById("emailp").innerHTML = document.getElementById(
+    "emailf"
+  ).value;
+
+  document.getElementById("mobilep").innerHTML = document.getElementById(
+    "mobf"
+  ).value;
+
+  document.getElementById("addressp").innerHTML = document.getElementById(
+    "addressf"
+  ).value;
+
+  document.getElementById("skillp").innerHTML = document.getElementById(
+    "skillf"
+  ).value;
+
+  document.getElementById("githubp").innerHTML = document.getElementById(
+    "githubf"
+  ).value;
+
+
+  document.getElementById("linkdinp").innerHTML = document.getElementById(
+    "linkdinf"
+  ).value;
+
+  // Role & objective
+  document.getElementById("rolep").innerHTML = document.getElementById(
+    "rolef"
+  ).value;
+  document.getElementById("objp").innerHTML = document.getElementById(
+    "objf"
+  ).value;
 
 
 
-  
-    // //direct
-  
-    // document.getElementById("namep").innerHTML = nameField;
-  
-    // //contact
-    // document.getElementById("mobilep").innerHTML = document.getElementById(
-    //   "MobField"
-    // ).value;
+  //we
 
-    // //contact
-    // document.getElementById("contactT").innerHTML = document.getElementById(
-    //     "contactField"
-    //   ).value;
-  
-    // //address
-    // document.getElementById("addressp").innerHTML = document.getElementById(
-    //   "addressField"
-    // ).value;
-    // document.getElementById("fbT").innerHTML = document.getElementById(
-    //   "fbField"
-    // ).value;
-    // document.getElementById("instaT").innerHTML = document.getElementById(
-    //   "instaField"
-    // ).value;
-    // document.getElementById("linkedT").innerHTML = document.getElementById(
-    //   "linkedField"
-    // ).value;
-  
-    //objective
-  
-    document.getElementById("objectiveT").innerHTML = document.getElementById(
-      "objectiveField"
-    ).value;
-  
-    //we
-  
-    let wes = document.getElementsByClassName("weField");
-  
-    let str = "";
-  
-    for (let e of wes) {
-      str = str + `<li> ${e.value} </li>`;
-    }
-  
-    document.getElementById("weT").innerHTML = str;
-  
-    //aq
-  
-    let aqs = document.getElementsByClassName("eqField");
-  
-    let str1 = "";
-  
-    for (let e of aqs) {
-      str1 += `<li> ${e.value} </li>`;
-    }
-  
-    document.getElementById("aqT").innerHTML = str1;
-  
-    //code for setting image
-  
-    let file = document.getElementById("imgField").files[0];
-  
-    console.log(file);
-  
-    let reader = new FileReader();
-  
-    reader.readAsDataURL(file);
-  
-    console.log(reader.result);
-  
-    //set the image to template
-  
-    reader.onloadend = function () {
-      document.getElementById("imgTemplate").src = reader.result;
-    };
-  
-    document.getElementById("cv-form").style.display = "none";
-    document.getElementById("cv-template").style.display = "block";
+  let wes = document.getElementsByClassName("weField");
+
+  let str = "";
+
+  for (let e of wes) {
+    str = str + `<li> ${e.value} </li>`;
   }
-  
-  //print cv
-  function printCV() {
-    window.print();
+
+  document.getElementById("weT").innerHTML = str;
+
+  //aq
+
+  let aqs = document.getElementsByClassName("eqField");
+
+  let str1 = "";
+
+  for (let e of aqs) {
+    str1 += `<li> ${e.value} </li>`;
   }
+
+  document.getElementById("aqT").innerHTML = str1;
+
+  //code for setting image
+
+  let file = document.getElementById("imgField").files[0];
+
+  console.log(file);
+
+  let reader = new FileReader();
+
+  reader.readAsDataURL(file);
+
+  console.log(reader.result);
+
+  //set the image to template
+
+  reader.onloadend = function () {
+    document.getElementById("imgTemplate").src = reader.result;
+  };
+
+  document.getElementById("cv-form").style.display = "none";
+  document.getElementById("cv-template").style.display = "block";
+}
+
+//print cv
+function printCV() {
+  window.print();
+}
